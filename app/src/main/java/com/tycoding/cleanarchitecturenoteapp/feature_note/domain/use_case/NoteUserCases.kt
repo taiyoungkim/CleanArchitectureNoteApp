72f66ff56e43621006faeb9ac23f4fbddb2ea49f
taiyoungkim <ktyoung153@gmail.com>
2022-09-18 20:17:21 +0900

    노트 data, domain 작업중

---
+++ app/src/main/java/com/tycoding/cleanarchitecturenoteapp/feature_note/domain/use_case/NoteUserCases.kt
@@ -0,0 +1,6 @@
+package com.tycoding.cleanarchitecturenoteapp.feature_note.domain.use_case
+
+data class NoteUserCases(
+    val getNotes: GetNotes,
+    val deleteNote: DeleteNote
+)
---
+++ app/src/main/java/com/tycoding/cleanarchitecturenoteapp/feature_note/domain/util/OrderType.kt
@@ -0,0 +1,6 @@
+package com.tycoding.cleanarchitecturenoteapp.feature_note.domain.util
+
+sealed class OrderType {
+    object Ascending: OrderType()
+    object Descending: OrderType()
+}
