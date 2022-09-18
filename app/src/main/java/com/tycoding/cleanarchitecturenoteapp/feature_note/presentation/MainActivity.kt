eac45cbc2d1f09f823e20a9bd5cc7e9d1bac9e2d
taiyoungkim <ktyoung153@gmail.com>
2022-09-18 16:21:18 +0900

    pakage divide for cleanArchitecture

--- app/src/main/java/com/tycoding/cleanarchitecturenoteapp/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/tycoding/cleanarchitecturenoteapp/feature_note/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.tycoding.cleanarchitecturenoteapp
+package com.tycoding.cleanarchitecturenoteapp.feature_note.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
