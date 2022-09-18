2709267df9c4451cf08e43ddbbfbadf1b3e7fb65
taiyoungkim <ktyoung153@gmail.com>
2022-09-18 20:25:35 +0900

    hilt di 추가

---
+++ app/src/main/java/com/tycoding/cleanarchitecturenoteapp/di/AppModule.kt
@@ -0,0 +1,45 @@
+package com.tycoding.cleanarchitecturenoteapp.di
+
+import android.app.Application
+import androidx.room.Room
+import com.tycoding.cleanarchitecturenoteapp.feature_note.data.data_source.NoteDatabase
+import com.tycoding.cleanarchitecturenoteapp.feature_note.data.repository.NoteRepositoryImpl
+import com.tycoding.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+import com.tycoding.cleanarchitecturenoteapp.feature_note.domain.use_case.DeleteNote
+import com.tycoding.cleanarchitecturenoteapp.feature_note.domain.use_case.GetNotes
+import com.tycoding.cleanarchitecturenoteapp.feature_note.domain.use_case.NoteUserCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDatabase {
+        return Room.databaseBuilder(
+            app,
+            NoteDatabase::class.java,
+            NoteDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NoteUserCases {
+        return NoteUserCases(
+            getNotes = GetNotes(repository),
+            deleteNote = DeleteNote(repository)
+        )
+    }
+}
