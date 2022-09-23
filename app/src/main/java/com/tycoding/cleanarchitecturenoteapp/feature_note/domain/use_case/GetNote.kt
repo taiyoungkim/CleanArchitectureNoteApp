973ea4b53624cf1587a480132949ad74c717eaba
taiyoungkim <ktyoung153@gmail.com>
2022-09-23 20:50:23 +0900

    AddEditNote ViewModel 작업 완료

---
+++ app/src/main/java/com/tycoding/cleanarchitecturenoteapp/feature_note/domain/use_case/GetNote.kt
@@ -0,0 +1,13 @@
+package com.tycoding.cleanarchitecturenoteapp.feature_note.domain.use_case
+
+import com.tycoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.tycoding.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+
+class GetNote(
+    private val repository: NoteRepository
+) {
+
+    suspend operator fun invoke(id: Int): Note? {
+        return repository.getNoteById(id)
+    }
+}
